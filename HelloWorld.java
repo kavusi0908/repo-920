3b5431363de44e1b12159d3885930063bbb6edc2
kavusi0908 <81158521+kavusi0908@users.noreply.github.com>
2021-03-24 20:06:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,9 @@
 class HelloWorld
 {
-  public void main(String []args)
+  public static void main(String []args)
   {
   Sytem.out.println("Hi Kavusi");
   }
   
-  public stativ void add(int num1,int num2)
-  {
-    System.out.println(num1+num2);
-  }
+
 }
